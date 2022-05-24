450283ef3523a2aca81f7fbb83585daf2b064d6b
Smartorin <75771645+Smartorin@users.noreply.github.com>
2022-05-23 19:47:52 -0600

    Revert "forkprueba"

--- lib/app/ui/paginas/home/principalController.dart
+++ lib/app/ui/paginas/home/principalController.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:parcial_4_electiva4/app/utilities/mapa_style.dart';
-import 'package:flutter/widgets.dart';
 
 class PrincipalController extends ChangeNotifier{
 
@@ -11,16 +10,21 @@ Map<MarkerId, Marker> _markers = {};
   const CameraPosition(target: LatLng(13.6845738,-89.2334234));
 
 Set<Marker> get markers => _markers.values.toSet();
+
 PrincipalController();
 
 void onMapCreated(GoogleMapController controller){
+
 controller.setMapStyle(Mapstyle);
+
 }
 
 void onTap(LatLng position){
   final markerId = MarkerId(_markers.length.toString());
   final maker = Marker(markerId: markerId, position: position);
+
 _markers[markerId] = maker;
 notifyListeners();
-  }
+
+}
 }
